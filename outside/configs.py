4c315dab03bd32b2a043ca4375c4760c0ae6be12
ezekiah <alexandreaazzouz@gmail.com>
2013-07-01 17:25:16 +0200

    sites correction

--- outside/configs.py
+++ outside/configs.py
@@ -3,6 +3,8 @@
 
 # context variables
 
+from django.conf import settings
+
 
 OUTSIDE_SITES_AVAILABLE = [
 	{
@@ -24,13 +26,13 @@ OUTSIDE_SITES_AVAILABLE = [
 		'long_content': 'archiPolis est un consortium <a href="http://www.corpus-ir.fr/" target="_blank">IR Corpus</a> créé pour inventorier, sauver les enquêtes anciennes et organiser la collecte de celles en cours et à venir en sciences sociales du politique. Il a aussi pour objet de proposer une définition élargie et contextualisée de l’enquête et des solutions de conservation pérenne.  Pour plus d’information sur archiPolis, <a href="http://quali.dime-shs.sciences-po.fr/archipolis/">cliquez ici</a>. '
 	},
 	{
-		'name':'app',
-		'url':'/app',
+		'name':'reanalyse',
+		'url':'/reanalyse' if settings.DEBUG == True else '/app',
 		'label':'enQ<span class="cyan">uêtes</span>',
 		'title':'l’instrument de DIME-SHS',
 		'content':'La banque d’enquêtes qualitatives',
 		'template':'hub',
-		'long_content': 'enQuêtes est l’un des trois instruments de l’Equipex DIME-SHS. Il fournit les ressources pour faire fonctionner la banque de mise à disposition des enquêtes (lien). La banque est organisée autour d’une approche holistique des enquêtes, via la construction d’un site par enquête, la mise en ligne d’une enquête sur l’enquête et le développement d’outils d’exploration. Pour plus d’informations sur enQuêtes <a href="http://quali.dime-shs.sciences-po.fr/app/">cliquez ici</a>.'
+		'long_content': 'enQuêtes est l’un des trois instruments de l’Equipex DIME-SHS. Il fournit les ressources pour faire fonctionner la banque de mise à disposition des enquêtes (lien). La banque est organisée autour d’une approche holistique des enquêtes, via la construction d’un site par enquête, la mise en ligne d’une enquête sur l’enquête et le développement d’outils d’exploration. Pour plus d’informations sur enQuêtes <a href="http://bequali.fr/app/">cliquez ici</a>.'
 		
 	},
 	{
