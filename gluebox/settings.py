0d07a4442e929eb645a2bdb4179e9055a1abb14a
Alexandre Aazzouz <alexandreaazzouz@gmail.com>
2014-06-06 14:54:46 +0200

    correction envoi mail

--- gluebox/settings.py
+++ gluebox/settings.py
@@ -24,6 +24,14 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+
+STAFF_EMAIL = 'guillaume.garcia@sciences-po.fr'
+EMAIL_HOST = 'smtp.sciences-po.fr'
+EMAIL_PORT = 25
+
+EMAIL_ADMINS = ['alexandre.aazzouz@sciences-po.fr', 'sarah.cadorel@sciences-po.fr', 'guillaume.garcia@sciences-po.fr', 'sophie.duchesne@sciences-po.fr', 'anne.both@sciences-po.fr', 'genevieve.michaud@sciences-po.fr']
+
+
 MANAGERS = ADMINS
 
 DATABASES = {
